aff2fa3a1716481cb0e2578855dae0dbc08c1055
HuuuuuuO <673225542@qq.com>
2024-06-11 10:06:27 +0800

    update

--- run_update.bat
+++ run_update.bat
@@ -1,4 +1,8 @@
+@REM git config --global user.name "HuuuuuuO"
+@REM git config --global user.email "673225542@qq.com"
+@REM git config user.name
+@REM git config user.email
 git add .
-git commit -m "up1"
+git commit -m "update"
 git push origin main
- 
+
